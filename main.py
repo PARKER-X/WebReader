24a6e0e5fbfa61d89bf87a7f8997b60dcdc8b2ed
Harsh Rawat <harshrawat096@gmail.com>
2025-05-20 11:40:35 +0530

    MCP Server!!

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import os
-import asynico
-from pydantic_ai import Agent
-from pydantic_ai.mcp import MCPServerStdio
+import asyncio
+from pydantic_ai import Agent # type: ignore
+from pydantic_ai.mcp import MCPServerStdio # type: ignore
+from dotenv import load_dotenv
+
+mcp_fetch_server = MCPServerStdio(
+    command="python",
+    args=["-m","mcp_server_fetch"]
+)
 
 
+agent = Agent(
+    model = "groq:\\llama-3.3-70b-versatile",
+    mcp_servers = [mcp_fetch_server]
+)
+
+async def main():
+    async with agent.run_mcp_servers():
+        result = await agent.run("extract the content and summarize it: https://jalammar.github.io/illustrated-transformer/ ")
+        output = result.output
+        return output
+
+
+if __name__ == "__main__":
+    output = asyncio.run(main())
+    print(output)
+    
